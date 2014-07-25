911c30ba75c78c4c5aa207e96fa4852ff4d52174
Melanie Gilman <melanie@thoughtbot.com>
2014-07-25 16:00:00 -0400

    Respond to feedback

--- luhn_validation.hs
+++ luhn_validation.hs
@@ -3,20 +3,19 @@ import Data.Char
 toDigits :: Int -> [Int]
 toDigits n
     | n <= 0    = []
-    | otherwise = map(\x -> digitToInt x) $ show n
+    | otherwise = map digitToInt $ show n
 
 doubleEveryOtherLeftToRight :: [Int] -> [Int]
 doubleEveryOtherLeftToRight [] = []
-doubleEveryOtherLeftToRight (x:xs) = x : (head xs * 2) : doubleEveryOtherLeftToRight(tail xs)
+doubleEveryOtherLeftToRight [x] = [x]
+doubleEveryOtherLeftToRight (x:y:xs) = x : y * 2 : doubleEveryOtherLeftToRight xs
 
 doubleEveryOther :: [Int] -> [Int]
-doubleEveryOther xs = reverse $ doubleEveryOtherLeftToRight $ reverse xs
+doubleEveryOther = reverse . doubleEveryOtherLeftToRight . reverse
 
 sumDigits :: [Int] -> Int
 sumDigits [] = 0
-sumDigits xs = sum . concat $ map (\x -> toDigits x) xs
+sumDigits xs = sum . concat $ map toDigits xs
 
 validate :: Int -> Bool
-validate n
-    | (sumDigits $ doubleEveryOther $ toDigits n) `mod` 10 == 0 = True
-    | otherwise = False
+validate n = (sumDigits $ doubleEveryOther $ toDigits n) `mod` 10 == 0
